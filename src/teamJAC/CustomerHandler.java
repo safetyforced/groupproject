f837b03188607d349d19b0ceb4afc14fa97343b2
Jamilah <jlwatt7@gmail.com>
2016-04-20 15:43:31 -0400

    created the class

--- src/teamJAC/CustomerHandler.java
+++ src/teamJAC/CustomerHandler.java
@@ -1,5 +1,35 @@
 package teamJAC;
- //mine
+
 public class CustomerHandler {
 
+	private String name;
+	private String email;
+	private int points;
+	
+	public int getPoints() {
+		return points;
+	}
+	public void setPoints(int points) {
+		this.points = points;
+	}
+	public CustomerHandler(String name, String email, int points) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.points = points;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String firstName) {
+		this.name = firstName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 }
