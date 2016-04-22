b1cfe2a1e463b91e6008094f0d4b389dffcfc4f5
Jamilah <jlwatt7@gmail.com>
2016-04-21 23:11:29 -0400

    Pushing changes

--- src/coffeeApp/myApp.java
+++ src/coffeeApp/myApp.java
@@ -9,49 +9,52 @@ public class myApp {
 	static ArrayList<CustomerHandler> list;
 	static int userPoints = 0;
 	static int userPosition = 0;
-public static void main(String[] args) {
+		
+	public static void main(String[] args) {
 
-    list=fileIOHandler.readData();	
+		list=fileIOHandler.readData();	
 	
-	System.out.println("Welcome to JAC'S Java!");
-	System.out.println();
+			System.out.println("Welcome to JAC'S Java!");
+			System.out.println();
 	
+		
+			System.out.print("Please enter your name:");
+			String userName = Scan1.nextLine();
+			userPosition = searchCustomer(userName);
+			userPoints = list.get(userPosition).getPoints();
+			System.out.println("Hello " + userName + "! You have " + userPoints + " points!");
 
-	System.out.print("Please enter your name:");
-	String userName = Scan1.nextLine();
-	userPosition = searchCustomer(userName);
-	userPoints = list.get(userPosition).getPoints();
-	System.out.println("Hello " + userName + "! You have " + userPoints + " points!");
+				
+				while (true)
+				{
+				System.out.println();
+				System.out.println("Menu:\n1) Add Points \n2) Redeem Points \n3) Exit App\nWhat would you like to do?");
+				int userInput = getValidNumberInRange(1,3);
+				System.out.println();
 
-	
-	while (true)
-	{
-	System.out.println();
-	System.out.println("Menu:\n1) Add Points \n2) Redeem Points \n3) Exit App\nWhat would you like to do?");
-	int userInput = getValidNumberInRange(1,3);
-	System.out.println();
-
-	
-if (userInput == 1){
-	userPoints++;
-	list.get(userPosition).setPoints(userPoints);
-} else if (userInput == 2) {
-	if (userPoints >= 10) {
-	userPoints -= 10;
-	list.get(userPosition).setPoints(userPoints);
-	} else {
-		System.out.println("Nice try, wiseguy.");
-		continue;
-	}
-} else if (userInput==3) 
-	{ System.out.println("Have a great Day!");
-	fileIOHandler.saveData(list);
-		break;
-	}
-
-	}
-	
-	
+							
+						if (userInput == 1){
+							userPoints++;
+							list.get(userPosition).setPoints(userPoints);
+							System.out.println("You now have " + userPoints + " points!");
+						}
+						else if (userInput == 2) {
+							if (userPoints >= 10) {
+							userPoints -= 10;
+							list.get(userPosition).setPoints(userPoints);
+							System.out.println("You now have " + userPoints + " points!");
+							} 
+						else {
+								System.out.println("Nice try, wiseguy.");
+								continue;
+						}
+						} 
+						else if (userInput==3) 
+						{ System.out.println("Have a great day!");
+						fileIOHandler.saveData(list);
+							break;
+						}
+					}	
 }
 	public static int readValidIntegerNoExp()
 	{ while (!Scan1.hasNextInt()) {
@@ -60,9 +63,8 @@ if (userInput == 1){
 			int x = Scan1.nextInt();
 			Scan1.nextLine();
 
-			return x;
-			
-	}//makes users enter valid number in range
+			return x;		
+	}
 	public static int getValidNumberInRange(int min, int max) {
 
 		int input = readValidIntegerNoExp();
@@ -83,8 +85,5 @@ public static int searchCustomer(String name){
 		}
 	}
 	return x;
-	
-	
-	
 }		
 }
